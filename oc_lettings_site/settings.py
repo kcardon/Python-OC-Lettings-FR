7fc18c157c01ddfa26b798b167b7eb179eecd703
kcardon <cardon.kevin@gmail.com>
2023-08-25 16:34:18 +0200

    handling secret key

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = env.SECRET_KEY
 SECRET_KEY = os.environ.get("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY must be set as an environment variable!")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
