da4dbff8f0ff6dc62dc045915a44ad03ca43acf1
kcardon <cardon.kevin@gmail.com>
2023-08-28 18:11:30 +0200

    fix: allowed host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "http://ocr-lettings-22191a719d2e.herokuapp.com/"]
 
 
 # Application definition
