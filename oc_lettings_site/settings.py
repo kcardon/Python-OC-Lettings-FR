8cffebdaeee151156d8c5b5ac287eed1a39f2266
kcardon <cardon.kevin@gmail.com>
2023-08-28 18:19:15 +0200

    allowed host update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "http://ocr-lettings-22191a719d2e.herokuapp.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
