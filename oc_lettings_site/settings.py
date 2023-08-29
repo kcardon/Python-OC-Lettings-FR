a75629f8cf542f38b19b1effff49667489b6d176
kcardon <cardon.kevin@gmail.com>
2023-08-29 09:35:45 +0200

    whitenoise handling static files

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "oc_lettings_site.urls"
@@ -126,7 +127,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 HANDLER404 = "oc_lettings_site.views.handler404"
 HANDLER500 = "oc_lettings_site.views.handler500"
 
