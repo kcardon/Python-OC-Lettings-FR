a263532feabcdd04c8566bff4605ff2a7e88ea1a
kcardon <cardon.kevin@gmail.com>
2023-09-04 14:46:55 +0200

    RdT config

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 project = 'lettings-app'
 copyright = '2023, kcardon'
@@ -14,7 +17,7 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
