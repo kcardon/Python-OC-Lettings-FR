df777db9592026fe2f58ab4d1a205e32ef3a86e6
kcardon <cardon.kevin@gmail.com>
2023-09-06 15:16:31 +0200

    fix conf.py theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '2.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc", "renku-sphinx-theme"]
+extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc", "renku_sphinx_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
